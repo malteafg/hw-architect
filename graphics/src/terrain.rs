b11b6043d187a56d48ca192015ffe837aa0517d5
malte <malteut@gmail.com>
2022-07-31 15:55:39 +0200

    terrain still not working

--- graphics/src/terrain.rs
+++ graphics/src/terrain.rs
@@ -39,45 +39,51 @@ pub struct TerrainMesh {
 
 impl TerrainMesh {
     pub fn new(device: &wgpu::Device) -> Self {
-        const color: [f32; 3] = [0.29, 0.61, 0.2];
-        const mapsize: usize = 1000;
-        const vertex_length: usize = mapsize + 1;
+        const COLOR: [f32; 3] = [0.29, 0.61, 0.2];
+        const MAP_SIZE: usize = 2;
+        const VERTEX_LENGTH: usize = MAP_SIZE + 1;
         let mut rng = rand::thread_rng();
-        let size = mapsize * mapsize * 6;
+        let size = MAP_SIZE * MAP_SIZE * 6;
 
-        let vertices = (0..vertex_length)
+        let vertices = (0..VERTEX_LENGTH)
             .flat_map(|x| {
-                (0..vertex_length)
+                (0..VERTEX_LENGTH)
                     .map(|y| {
                         let r: f32 = rng.gen::<f32>() * 0.06 + 0.97;
                         TerrainVertex {
                             position: [
-                                ((0 - (mapsize as i32) / 2) + (x as i32)) as f32,
-                                0.0,
-                                ((0 - (mapsize as i32) / 2) + (y as i32)) as f32,
+                                ((0 - (MAP_SIZE as i32) / 2) + (x as i32)) as f32,
+                                2.0,
+                                ((0 - (MAP_SIZE as i32) / 2) + (y as i32)) as f32,
                             ],
-                            color: [color[0] * r, color[1] * r, color[2] * r],
+                            color: [COLOR[0] * r, COLOR[1] * r, COLOR[2] * r],
                         }
                     })
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
 
-        let indices = (0..mapsize)
+        let indices = (0..MAP_SIZE)
             .flat_map(|x| {
-                (0..mapsize)
+                (0..MAP_SIZE)
                     .map(|y| {
                         [
-                            x * vertex_length + y,
-                            x * vertex_length + y + vertex_length,
-                            x * vertex_length + y + vertex_length + 1,
-                            x * vertex_length + y + vertex_length + 1,
-                            x * vertex_length + y + 1,
-                            x * vertex_length + y,
+                            [
+                                x * VERTEX_LENGTH + y,
+                                x * VERTEX_LENGTH + y + VERTEX_LENGTH,
+                                x * VERTEX_LENGTH + y + VERTEX_LENGTH + 1,
+                            ],
+                            [
+                                x * VERTEX_LENGTH + y + VERTEX_LENGTH + 1,
+                                x * VERTEX_LENGTH + y + 1,
+                                x * VERTEX_LENGTH + y,
+                            ],
                         ]
                     })
                     .collect::<Vec<_>>()
             })
+            .flatten()
+            .flatten()
             .collect::<Vec<_>>();
         // let mut vertices = [0.0; vertex_length * vertex_length * 6];
         // let mut indices = [0; mapsize * mapsize * 6];
@@ -103,6 +109,9 @@ impl TerrainMesh {
         //     }
         // }
 
+        dbg!(vertices.clone());
+        dbg!(indices.clone());
+        println!("num verts: {}", vertices.len());
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Terrain Vertex Buffer"),
             contents: bytemuck::cast_slice(&vertices),
@@ -117,7 +126,7 @@ impl TerrainMesh {
         TerrainMesh {
             vertex_buffer,
             index_buffer,
-            size,
+            size: size as usize,
         }
     }
 }
