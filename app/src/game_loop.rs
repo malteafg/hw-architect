e67acb88b49f8f3a985d32d1aa57f45f0d97fa7e
malte <malteut@gmail.com>
2024-07-14 20:48:24 +0200

    It is compiling nicely

--- app/src/game_loop.rs
+++ app/src/game_loop.rs
@@ -10,8 +10,6 @@ use winit::{
     window::WindowBuilder,
 };
 
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::time::Instant;
 
 pub async fn run() {
@@ -28,6 +26,7 @@ pub async fn run() {
     // create event_loop and window
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let this_window_id = window.id();
     window.set_title("Highway Architect");
     window.set_min_inner_size(Some(PhysicalSize::new(window_width, window_height)));
     window.set_outer_position(PhysicalPosition::new(0, 0));
@@ -35,68 +34,72 @@ pub async fn run() {
     // Create handle to graphics card. Change line to use different gpu backend.
     let gfx = gfx_wgpu::GfxState::new(&window, window_width, window_height).await;
 
-    let mut state = state::State::new(
-        Rc::new(RefCell::new(gfx)),
-        window_width,
-        window_height,
-        input_handler,
-    );
+    let mut state = state::State::new(gfx, &window, window_width, window_height, input_handler);
+    let mut surface_configured = false;
 
     let mut last_render_time = Instant::now();
-    event_loop.run(move |event, window_target| {
-        window_target.set_control_flow(ControlFlow::Poll);
-        use input::Action;
-        use input_handler::InputEvent;
-        match state.input_handler.process_input(&event, window.id()) {
-            InputEvent::KeyActions(actions) => {
-                if actions.contains(&(Action::Exit, input::KeyState::Press)) {
-                    window_target.exit();
-                } else {
-                    for a in actions {
-                        // dbg!(a.clone());
-                        state.key_input(a);
+    event_loop
+        .run(move |event, window_target| {
+            window_target.set_control_flow(ControlFlow::Poll);
+            use input::Action;
+            use input_handler::InputEvent;
+            match state.input_handler.process_input(&event, this_window_id) {
+                InputEvent::KeyActions(actions) => {
+                    if actions.contains(&(Action::Exit, input::KeyState::Press)) {
+                        window_target.exit();
+                    } else {
+                        for a in actions {
+                            // dbg!(a.clone());
+                            state.key_input(a);
+                        }
                     }
                 }
-            }
-            InputEvent::MouseEvent(e) => state.mouse_input(e),
-            InputEvent::Absorb => {}
-            InputEvent::Proceed => match event {
-                Event::Resumed => window.request_redraw(),
-                Event::WindowEvent { event, window_id } if window_id == window.id() => {
-                    match event {
-                        #[cfg(not(target_arch = "wasm32"))]
-                        WindowEvent::CloseRequested => window_target.exit(),
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(physical_size.width, physical_size.height);
-                        }
-                        WindowEvent::RedrawRequested => {
-                            let now = Instant::now();
+                InputEvent::MouseEvent(e) => state.mouse_input(e),
+                InputEvent::Absorb => {}
+                InputEvent::Proceed => match event {
+                    Event::WindowEvent { event, window_id } if window_id == this_window_id => {
+                        match event {
+                            #[cfg(not(target_arch = "wasm32"))]
+                            WindowEvent::CloseRequested => window_target.exit(),
+                            WindowEvent::Resized(physical_size) => {
+                                surface_configured = true;
+                                state.resize(physical_size.width, physical_size.height);
+                            }
+                            WindowEvent::RedrawRequested => {
+                                state.window().request_redraw();
 
-                            let dt = now - last_render_time;
-                            last_render_time = now;
-                            state.update(dt);
-                            let render_error = state.render();
-
-                            use gfx_api::GfxFrameError;
-                            match render_error {
-                                Ok(_) => {}
-                                // Reconfigure the surface if it's lost or outdated
-                                Err(GfxFrameError::Lost | GfxFrameError::Outdated) => {
-                                    state.redraw();
+                                if !surface_configured {
+                                    return;
                                 }
-                                // The system is out of memory, we should probably quit
-                                Err(GfxFrameError::OutOfMemory) => window_target.exit(),
-                                // We're ignoring timeouts
-                                Err(GfxFrameError::Timeout) => {
-                                    log::warn!("Surface timeout")
+
+                                let now = Instant::now();
+
+                                let dt = now - last_render_time;
+                                last_render_time = now;
+                                state.update(dt);
+                                let render_error = state.render();
+
+                                use gfx_api::GfxFrameError;
+                                match render_error {
+                                    Ok(_) => {}
+                                    // Reconfigure the surface if it's lost or outdated
+                                    Err(GfxFrameError::Lost | GfxFrameError::Outdated) => {
+                                        state.redraw();
+                                    }
+                                    // The system is out of memory, we should probably quit
+                                    Err(GfxFrameError::OutOfMemory) => window_target.exit(),
+                                    // We're ignoring timeouts
+                                    Err(GfxFrameError::Timeout) => {
+                                        log::warn!("Surface timeout")
+                                    }
                                 }
                             }
+                            _ => {}
                         }
-                        _ => {}
                     }
-                }
-                _ => {}
-            },
-        }
-    });
+                    _ => {}
+                },
+            }
+        })
+        .unwrap();
 }
--- gfx-wgpu/src/primitives/texture.rs
+++ gfx-wgpu/src/primitives/texture.rs
@@ -37,11 +37,8 @@ impl Texture {
             address_mode_v: wgpu::AddressMode::ClampToEdge,
             address_mode_w: wgpu::AddressMode::ClampToEdge,
             mag_filter: wgpu::FilterMode::Linear,
-            min_filter: wgpu::FilterMode::Linear,
-            mipmap_filter: wgpu::FilterMode::Linear,
-            compare: Some(wgpu::CompareFunction::LessEqual),
-            lod_min_clamp: -100.0,
-            lod_max_clamp: 100.0,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
             ..Default::default()
         });
 
